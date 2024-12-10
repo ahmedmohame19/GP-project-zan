e05f332eb4b04d154afb434680f1f554ed22bac4
ahmedmohame19 <96324257+ahmedmohame19@users.noreply.github.com>
2024-12-11 00:44:27 +0200

    finsh intro

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import "./Landing.scss"
+import { GoSearch } from "react-icons/go";
 export default function Landing() {
   return <>
     <div className="main-landing">
-      <div className="text"></div>
-      <div className="sub-text"></div>
+      <div className="text">
+        <h1>Make Your <span>Interior</span>  More</h1>
+        <h2>Minimalistic & Modern</h2>
+        <p>
+          Turn your room with Zan into a lot more minimalist and modern with ease and speed
+        </p>
+        <div className="search">
+          <GoSearch className='rounded-full' />
+          <input type="search" placeholder='search furniture' className='rounded-full' />
+        </div>
+      </div>
     </div>
 
   </>
